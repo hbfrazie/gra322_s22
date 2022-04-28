5327d5250bd4807582fbc3595539bd27476c66b5
hayden frazier <Haydenfrazier22@yahoo.com>
2022-04-28 16:37:52 -0700

    Charlies Website
    Charlies Website Final

---
+++ charlies-site/js/scripts.js
@@ -0,0 +1,15 @@
+var slider = tns({
+    container: '.my-slider',
+    items: 2,
+    slideBy: 'page',
+    autoplay: true,
+    controls: false,
+    nav: false,
+    autoplayButtonOutput: false,
+    gutter: 35,
+    center: true,
+  });
+
+  
+
+ 
